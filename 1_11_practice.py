34317c86bbd17ba7f1193735a778c8dbe55616ae
Sohum Tiwary <sohum.tiwary@gmail.com>
2024-01-12 23:18:01 -0800

    1_12 update

--- 1_11_practice.py
+++ 1_11_practice.py
@@ -4,5 +4,5 @@ for i in range(5):
 arr = [i for i in range(5)]
 # print(arr)
 
-two_dimensional_array = [[5]*4 for i in range(5)]
+two_dimensional_array = [[7]*4 for i in range(5)]
 print(two_dimensional_array)
