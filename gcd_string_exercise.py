c0105563a1c9d38ebe4c5588bbe5d22348994844
Sohum Tiwary <sohum.tiwary@gmail.com>
2024-01-15 15:43:50 -0800

    fixing the code

--- gcd_string_exercise.py
+++ gcd_string_exercise.py
@@ -1,19 +1,14 @@
 str1="ABCABC"
 str2="ABC"
-output = []
 
-arr1 = [i for i in str1]
-arr2 = [i for i in str2]
+def main():
+    print(gcdOfStrings(str1, str2))
 
-i = 0
+def gcdOfStrings(str1: str, str2: str) -> str:
+    if str1 + str2 != str2 + str1:
+        return ""
 
-while i < len(str1) and i < len(str2):
-    if arr1[i] == arr2[i]:
-        arr1[i] = 0
-        arr2[i] = 0
-    else:
-        output.append(arr1[i])
+    from math import gcd
+    return str1[:gcd(len(str1), len(str2))]
 
-    i += 1
-
-print(''.join(output))
+main()
